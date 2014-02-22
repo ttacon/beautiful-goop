0afdc93b2bc8fc3091561ebe8ffbd9fcd0444986
ttacon <ttacon@gmail.com>
2014-02-22 11:16:46 -0500

    Add part of nodeEqual and another test

--- goop/node_test.go
+++ goop/node_test.go
@@ -1,6 +1,10 @@
 package goop
 
-import "testing"
+import (
+	"code.google.com/p/go.net/html"
+	"strings"
+	"testing"
+)
 
 func Test_FindAllElement(t *testing.T) {
 
@@ -10,12 +14,114 @@ func Test_NewGoopNode(t *testing.T) {
 
 }
 
-func Test_BuildGoop(t *testing.T) {
+type goopNodeTest struct {
+	input string
+	node  *GoopNode
+}
 
+type goopTest struct {
+	input string
+	goop  *Goop
+}
+
+func Test_BuildGoop(t *testing.T) {
+	parent := &html.Node{
+		Type: 0x2,
+	}
+	child := &html.Node{
+		Type:     0x3,
+		DataAtom: 0x27604,
+		Data:     "html",
+	}
+	head := &html.Node{
+		Parent:   child,
+		Type:     0x3,
+		DataAtom: 0x2fa04,
+		Data:     "head",
+	}
+
+	body := &html.Node{
+		Parent:      child,
+		PrevSibling: head,
+		Type:        0x3,
+		DataAtom:    0x2f04,
+		Data:        "body",
+	}
+	head.NextSibling = body
+
+	div := &html.Node{
+		Parent:   body,
+		Type:     0x3,
+		DataAtom: 0x10703,
+		Data:     "div",
+	}
+	body.FirstChild = div
+	body.LastChild = div
+
+	foo := &html.Node{
+		Parent: div,
+		Type:   0x1,
+		Data:   "Foo",
+	}
+	div.FirstChild = foo
+	div.LastChild = foo
+
+	parent.FirstChild = child
+	parent.LastChild = child
+	child.FirstChild = head
+	child.LastChild = body
+	child.Parent = parent
+
+	tests := []goopTest{
+		goopTest{
+			"<div>Foo</div>",
+			&Goop{Root: &GoopNode{parent}},
+		},
+	}
+
+	for _, test := range tests {
+		g, err := BuildGoop(strings.NewReader(test.input))
+
+		if err != nil {
+			t.Errorf("error occured while building some tasty goop: %v", err)
+			continue
+		}
+
+		if !nodeEqual(test.goop.Root.Node, g.Root.Node) {
+			t.Errorf("goop built: %v doesnt match expected %v\n", g, test.goop)
+		}
+	}
+}
+
+func nodeEqual(n1, n2 *html.Node) bool {
+	if n1 == nil || n2 == nil {
+		return true
+	}
+	if (n1 != nil && n2 == nil) || (n1 == nil && n2 != nil) {
+		return false
+	}
+
+	// TODO(ttacon): go through node's own siblings
+
+	c1 := n1.FirstChild
+	c2 := n2.FirstChild
+	for c1 != nil && c2 != nil {
+		if c1 == nil || c2 == nil {
+			return false
+		}
+		if !nodeEqual(c1, c2) {
+			return false
+		}
+		c1 = c1.NextSibling
+		c2 = c2.NextSibling
+	}
+
+	return n1.Type == n2.Type &&
+		n1.Data == n2.Data &&
+		n1.DataAtom == n2.DataAtom
 }
 
 func Test_GoopFind(t *testing.T) {
-
 }
 
 type tokenizeTest struct {
