ce051088ee758291790a1b6df1f4536810ec8fd1
ttacon <ttacon@gmail.com>
2014-02-22 09:33:17 -0500

    Add tests

---
+++ goop/node_test.go
@@ -0,0 +1,67 @@
+package goop
+
+import "testing"
+
+func Test_FindAllElement(t *testing.T) {
+
+}
+
+func Test_NewGoopNode(t *testing.T) {
+
+}
+
+func Test_BuildGoop(t *testing.T) {
+
+}
+
+func Test_GoopFind(t *testing.T) {
+
+}
+
+func Test_tokenize(t *testing.T) {
+
+}
+
+func Test_GoopNodeFind(t *testing.T) {
+
+}
+
+func Test_GoopNodeHasClasses(t *testing.T) {
+
+}
+
+func Test_GoopNodeIsElement(t *testing.T) {
+
+}
+
+func Test_GoopFindAllElements(t *testing.T) {
+
+}
+
+func Test_GoopNodeFindAllElements(t *testing.T) {
+
+}
+
+func Test_GoopNodeSearchByElement(t *testing.T) {
+
+}
+
+func Test_GoopFindAllWithClass(t *testing.T) {
+
+}
+
+func Test_GoopNodeSearchByClass(t *testing.T) {
+
+}
+
+func Test_GoopFindById(t *testing.T) {
+
+}
+
+func Test_GoopNodeFindById(t *testing.T) {
+
+}
+
+func Test_Attributes(t *testing.T) {
+
+}
